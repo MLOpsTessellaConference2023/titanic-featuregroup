7e6802432ed3c5c6f6f2f29157fcad0aceaaba3c
jose-juan.pena-gomez@capgemini.com <jose.penas@securitasdirect.es>
2023-01-27 16:08:47 +0100

    change: pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -11,34 +11,32 @@ Notes
 Date: 01/2023
 Author: (C) Capgemini Engineering - Antonio Galan, Jose Pena
 """
+import sagemaker
 from sagemaker.workflow.pipeline_context import LocalPipelineSession
-from sagemaker.pytorch import PyTorch
-from sagemaker.workflow.steps import TrainingStep
+from sagemaker.workflow.steps import Step, StepTypeEnum
 from sagemaker.workflow.pipeline import Pipeline
 
-local_pipeline_session = LocalPipelineSession()
+pipeline_session = LocalPipelineSession()
 
-pytorch_estimator = PyTorch(
-    sagemaker_session=local_pipeline_session,
-    role=sagemaker.get_execution_role(),
-    instance_type="ml.c5.xlarge",
-    instance_count=1,
-    framework_version="1.8.0",
-    py_version="py36",
-    entry_point="./entry_point.py",
-)
-
-step = TrainingStep(
-    name="MyTrainingStep",
-    step_args=pytorch_estimator.fit(
-        inputs=TrainingInput(s3_data="s3://my-bucket/my-data/train"),
-    )
-)
+steps = [
+    Step(
+        name="WrangleData",
+        step_type=StepTypeEnum.PROCESSING,
+    ),
+    Step(
+        name="FeatureEngineering",
+        step_type=StepTypeEnum.PROCESSING,
+    ),
+    Step(
+        name="IngestFeatureGroup",
+        step_type=StepTypeEnum.PROCESSING,
+    ),
+]
 
 pipeline = Pipeline(
-    name="MyPipeline",
-    steps=[step],
-    sagemaker_session=local_pipeline_session
+    name="titanic-featuregroup",
+    steps=steps,
+    sagemaker_session=pipeline_session
 )
 
 pipeline.create(
@@ -51,6 +49,7 @@ execution = pipeline.start()
 
 steps = execution.list_steps()
 
+## FIXME
 training_job_name = steps['PipelineExecutionSteps'][0]['Metadata']['TrainingJob']['Arn']
 
-step_outputs = pipeline_session.sagemaker_client.describe_training_job(TrainingJobName = training_job_name)
+step_outputs = pipeline_session.sagemaker_client.describe_training_job(TrainingJobName=training_job_name)
