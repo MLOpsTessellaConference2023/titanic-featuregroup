e2c8432116b0c635fa761ef7b797f9f638ac4388
jpenagom <jose-juan.pena-gomez@capgemini.com>
2023-02-09 11:41:09 +0100

    change: header format

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Date: 01/2023
+Version: 1.0
+Author: (C) Capgemini Engineering - Antonio Galan, Jose Pena
+Website: www.capgemini.com
+
+"""
--- src/data_wrangling.py
+++ src/data_wrangling.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Date: 01/2023
+Version: 1.0
 Author: (C) Capgemini Engineering - Antonio Galan, Jose Pena
+Website: www.capgemini.com
+
 
 Data Wrangling
 ==============
